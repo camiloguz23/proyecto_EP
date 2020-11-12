a9cead328b86bb7d8bd616e2bd6af6e990b3bc62
Holwan david montes <holmanmontesmontes57@gmail.com>
2020-11-12 12:44:13 -0500

    subiendo al coneccion a la base de datos e ingresando un archivo php para el ingreso de los usuarios

---
+++ php/ingreso.php
@@ -0,0 +1,51 @@
+<?php
+
+    session_start();
+
+    require 'connecion.php';   
+            
+            $usuario = $_POST["usuario"];
+            $clave = $_POST["clave"];
+           
+            
+                $consul = "SELECT * FROM usuario WHERE usuario = '$usuario'  and clave = '$clave'";
+               
+                $query=mysqli_query($connection, $consul);
+                $fila = mysqli_fetch_assoc($query);
+
+              
+
+                        if($fila){
+                            $_SESSION['id_tip_usu'] = $fila['id_tip_user'];
+                    
+                            $_SESSION['usuario'] = $fila['usuario'];
+                           
+                            
+
+                                if($_SESSION["id_tip_usu"] == 1 ){
+                                    
+                                    header("location: ../instru/instructor.html");
+                                    exit();
+                                }
+                                elseif($_SESSION["id_tip_usu"] == 2 ){
+                                    header("location: ../segui/segui.html");
+                                    exit();
+                                }
+                                 else{
+                                    echo "error ";
+                                    exit();
+                                }  
+                                
+                        } else{
+                            
+                            echo "error ";
+                                exit();
+                        }  
+                                                
+        
+                      
+
+?>     
+
+
+
