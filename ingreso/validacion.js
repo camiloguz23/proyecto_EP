fe820cc25bd760e79cdfe101f8b40ec72edac0d8
Holwan david montes <holmanmontesmontes57@gmail.com>
2020-11-16 15:57:04 -0500

    arreglo de un formulario atravesado y otras cosas con js

---
+++ ingreso/validacion.js
@@ -0,0 +1,32 @@
+/* const documento = document.getElementById("usuario")
+const clave = document.getElementById("password");
+const form = document.getElementById("form1");
+const aviso = document.getElementById("aviso");
+
+/* form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    if(documento.value.length < 6){
+       warnings += `El documento no es valido <br>`
+       entrar = true
+    }
+    if(clave.value.length < 10){
+        e.preventDefault()
+        warnings += `la contrasña tiene que tenerminimo 10 caracteres <br>`
+        entrar = true
+    }
+    
+    if(entrar){
+        e.preventDefault()
+        aviso.innerHTML=warnings
+    }
+})
+ */ 
+
+ 
+
+
+
+
+
---
+++ segui/validacionF.js
@@ -0,0 +1,10 @@
+const fomulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input'); //elijo el id formulario y que me seleccione todos los input
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras min-mayus, numeros del 0-9, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z-]+\.[a-zA-Z-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
