ab4d96c87e1ef25058cd1d4b2025bee894ab6de5
Harol Guzman <harol.stiven147@gmail.com>
2020-11-24 17:11:30 -0500

    Cambios menores a biblioteca

--- paz_y_salvo/pazysalvo.php
+++ paz_y_salvo/pazysalvo.php
@@ -61,7 +61,7 @@ if(isset($_POST)){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Paz y salvo</title>
     <link rel="stylesheet" href="style/pazysalvo.css">
     <link rel="icon" href="../imagenes/favicon.ico">
 </head>
