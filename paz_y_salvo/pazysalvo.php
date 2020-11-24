545584351ca6e8dbc5cd957bc4112e4e0716b49f
Harol Guzman <harol.stiven147@gmail.com>
2020-11-24 18:28:09 -0500

    Arreglos al boton de firma y validación de firma

--- paz_y_salvo/pazysalvo.php
+++ paz_y_salvo/pazysalvo.php
@@ -277,45 +277,8 @@ if(isset($_POST)){
     </div>
 <script>
     const id_tip_usu = "<?php echo $_SESSION['id_tip_usu'];?>"
-    const btn_ape = document.getElementById("Agenciapublicadeempleo");
-    const btn_biblioteca = document.getElementById("Biblioteca");
-    const btn_coordinador = document.getElementById("CoordinadorAcademico");
-    const btn_bienestar = document.getElementById("Bienestar");
-    const btn_imprimir = document.getElementById("btn_imprimir");
-    const acciones = document.getElementsByClassName("desaparecer");
-
-    btn_imprimir.addEventListener("click", (e) => {
-        e.preventDefault();
-        print();
-    })
-    
-    switch(id_tip_usu) {
-        case "4": 
-            btn_biblioteca.style.visibility = "hidden";
-            btn_coordinador.style.visibility = "hidden";
-            btn_bienestar.style.visibility = "hidden";
-            btn_ape.style.visibility = "visible";
-            break;
-        case "5": 
-            btn_biblioteca.style.visibility = "hidden";
-            btn_coordinador.style.visibility = "hidden";
-            btn_ape.style.visibility = "hidden";
-            btn_bienestar.style.visibility = "visible";
-            break;
-        case "6": 
-            btn_biblioteca.style.visibility = "hidden";
-            btn_ape.style.visibility = "hidden";
-            btn_bienestar.style.visibility = "hidden";
-            btn_coordinador.style.visibility = "visible";
-            break;
-        case "7": 
-            btn_ape.style.visibility = "hidden";
-            btn_coordinador.style.visibility = "hidden";
-            btn_bienestar.style.visibility = "hidden";
-            btn_biblioteca.style.visibility = "visible";
-            break;
-    }
 </script>
+<script src="script/pazysalvo.js"></script>
 </body>
 </html>
 
