c04c327755c24beb53cb6ecd44ac96ed80eb03b4
stefania herrera <herrerastefania904@gmail.com>
2020-11-25 13:00:44 -0500

    instru (consultas)

--- instru/evidencias.php
+++ instru/evidencias.php
@@ -6,12 +6,14 @@ $apre = $_POST["documenapre"];
 $cheq =$_POST["check"];
 
 echo($ins);
+$consul = "UPDATE detalle_formacion INNER JOIN calificacion on detalle_formacion.id_aprend=calificacion.id_aprend SET id_estado=3  WHERE calificacion.id_esta_evi=2 ";
+$sqli = mysqli_query($connection, $consul);
 
-$evidencia = "UPDATE calificacion SET id_esta_evi=2,id_doc_instru=$ins,fecha_cali=now() WHERE `id_evidencias`=$cheq ";
-$sql = mysqli_query($connection, $evidencia);
 
-if($sql)
+if($sqli)
 {
+    $evidencia = "UPDATE calificacion SET id_esta_evi=2,id_doc_instru=$ins,fecha_cali=now() WHERE `id_evidencias`=$cheq ";
+    $sql = mysqli_query($connection, $evidencia);
     
     header("location: instructor.php");
                             
