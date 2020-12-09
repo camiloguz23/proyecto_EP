ad261926fa6c6fa796698cad1e41cad98a2d6ef0
camilo <jcguzman8548@misena.edu.co>
2020-12-09 11:46:18 -0500

    fotos del perfil terminados

--- segui/segui.php
+++ segui/segui.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $usario = $_SESSION["documento"];
-// if ($usario == "" || $usario == null) {
-//     header("location: ../index.html");
-// }
+if ($usario == "" || $usario == null) {
+    header("location: ../index.html");
+}
 require_once('../php/connecion.php');
 
 ?>
